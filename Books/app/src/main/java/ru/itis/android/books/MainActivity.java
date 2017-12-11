7de4fae78ad40620fef12a3d9e88f27ab5294998
Valera <valera0798@mail.ru>
2017-12-11 16:10:59 +0300

    Init

---
+++ Books/app/src/main/java/ru/itis/android/books/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.itis.android.books;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+        // s
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
